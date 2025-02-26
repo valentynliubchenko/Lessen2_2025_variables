3ea98ca8657b34655b534ee4cc801e440a386154
valentynliubchenko <luybchenko.val@gmail.com>
2025-02-26 15:42:45 +0200

    update stringBuilder

--- stringexamples/src/main/java/ua/nure/AppStringCamelCase.java
+++ stringexamples/src/main/java/ua/nure/AppStringCamelCase.java
@@ -3,17 +3,19 @@ package ua.nure;
 public class AppStringCamelCase {
     public static void main(String[] args) {
         String input = "Hello,    woRTld  ,  java     programming!"; //"HelloWorldJavaProgramming";
+
         System.out.println("Origina: "+input);
         System.out.println("Hex: "+Integer.toHexString(input.hashCode()));
-        String result ="";
+//        String result ="";
+        StringBuilder result = new StringBuilder();
         String[] words = input.split("[,\\s!]+");
         for (String word : words) {
-            result += word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
-            System.out.println("Current result: "+result);
+            result.append(word.substring(0,1).toUpperCase()).append(word.substring(1).toLowerCase());
+            System.out.println("Current result: "+result.toString());
             System.out.println("Curent Hex: "+Integer.toHexString(result.hashCode()));
         }
 
-        System.out.println("result: "+result);
+        System.out.println("result: "+result.toString());
         System.out.println("Hex: "+Integer.toHexString(result.hashCode()));
 
     }
