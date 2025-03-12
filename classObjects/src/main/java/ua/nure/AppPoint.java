45b43338870f1c9f18b0a0ca8044dfa1bfea45ad
valentynliubchenko <luybchenko.val@gmail.com>
2025-03-12 16:09:42 +0200

    Example record2

--- classObjects/src/main/java/ua/nure/AppPoint.java
+++ classObjects/src/main/java/ua/nure/AppPoint.java
@@ -7,13 +7,15 @@ public class AppPoint
 {
     public static void main( String[] args )
     {
-        MyPoint myPoint = new MyPoint(5,6);
+        MyPoint myPoint = new MyPoint(115,6);
 
         log.error("{} {} {}", myPoint.x(), myPoint.y(), myPoint.hashCode());
 
         myPoint = new MyPoint(15,26);
 
         log.error("{} {} {}", myPoint.x(), myPoint.y(), myPoint.hashCode());
-
+        log.error("{}", myPoint);
+        MyPoint myPoint2 = MyPoint.create();
+        log.error("{}", myPoint2);
     }
 }
--- classObjects/src/main/java/ua/nure/MyPoint.java
+++ classObjects/src/main/java/ua/nure/MyPoint.java
@@ -1,4 +1,12 @@
 package ua.nure;
 
 public record MyPoint(int x, int y) {
+
+    @Override
+    public String toString() {
+        return String.format("my string: x = %d  y = %d" , x, y );
+    }
+    public static MyPoint create(){
+        return new MyPoint(100, 100);
+    }
 }
