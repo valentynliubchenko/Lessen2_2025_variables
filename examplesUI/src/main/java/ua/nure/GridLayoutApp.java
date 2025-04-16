64fc9cd288e1346a79fb4c5743e389a17d0a934f
valentynliubchenko <lubchenko.val@gmail.com>
2025-04-16 15:48:12 +0300

    GridLayout

--- examplesUI/src/main/java/ua/nure/GridLayoutApp.java
+++ examplesUI/src/main/java/ua/nure/GridLayoutApp.java
@@ -3,13 +3,17 @@ package ua.nure;
 import javax.swing.*;
 import java.awt.*;
 
-public class GridLayoutApp
-{
-    public static void main( String[] args )
-    {
+public class GridLayoutApp {
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame();
             frame.setSize(300, 300);
+
+//            JPanel panel = new JPanel();
+//            panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); // вертикальне розміщення
+//            panel.add(new JButton("Верх"));
+//            panel.add(new JButton("Середина"));
+//            panel.add(new JButton("Низ"));
 
             JPanel panel = new JPanel(new GridLayout(2, 3)); // 2 рядки, 3 стовпці
             for (int i = 1; i <= 7; i++) {
