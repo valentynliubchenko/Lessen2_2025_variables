9dfd34e3676f4b9c31848bd3b507e9b820bba5f6
valentynliubchenko <lubchenko.val@gmail.com>
2025-04-23 16:16:12 +0300

    Added base Server ObjectInputStream

---
+++ socketExamples/src/main/java/ua/nure/ClientObjectInputStream.java
@@ -0,0 +1,32 @@
+package ua.nure;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ClientObjectInputStream {
+    public static void main(String[] args) {
+        int port =8083;
+        Person person = new Person("Микола", 25);
+
+        System.out.println( "Client is starting..." );
+        try {
+            Socket socket = new Socket(InetAddress.getLocalHost(), port);
+            socket.setSoTimeout(1000);
+            System.out.println( "Client is connected to " + socket.getRemoteSocketAddress() + "port " + socket.getPort() );
+
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+
+            out.writeObject(person);
+            out.flush();
+
+
+            System.out.println( "sent data person : " + person.toString() );
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ socketExamples/src/main/java/ua/nure/ServerObjectInputStream.java
@@ -0,0 +1,42 @@
+package ua.nure;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerObjectInputStream {
+    public static void main(String[] args) {
+        System.out.println("Server is starting!");
+        {
+            try (ServerSocket serverSocket = new ServerSocket(8082)) {
+                while (true) {
+                    Socket socket = serverSocket.accept();
+
+                    ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+                    try {
+                        Object receivedObject = ois.readObject();
+                        if (receivedObject instanceof Person) {
+                            Person person = (Person) receivedObject;
+                            System.out.println("Receive person:" + person);
+                        } else {
+                            System.out.println("Received object is not of type Person");
+                        }
+
+
+                    } catch (ClassNotFoundException e) {
+                        System.out.println("Class not found");
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                //System.out.println("Server accepted!");
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
